669d803e58e9bf699d5b9137268d326a6cb419bc
Parv Jain <parvjain12@protonmail.com>
2024-01-03 12:07:33 +0530

    error handle added

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 var cors = require("cors");
+const cookieParser = require("cookie-parser")
+const errorHandler = require("./middleware/error");
 
 //database connection
 mongoose.connect(process.env.DATABASE, {
@@ -15,6 +17,20 @@ mongoose.connect(process.env.DATABASE, {
 })
     .then(() => console.log("DB connected"))
     .catch((err) => console.log(err));
+
+//MIDDLEWARE
+app.use(morgan('dev'));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true
+}));
+app.use(cookieParser());
+app.use(cors());
+
+// error middleware
+app.use(errorHandler);
+
 
 //port
 const port = process.env.PORT ||8000
