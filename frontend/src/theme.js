31e9230a25155916e40e5a9406c549b69da2e3a4
Parv Jain <parvjain12@protonmail.com>
2024-01-07 14:09:13 +0530

    started with frontend added some basic react library

---
+++ frontend/src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+// import { red, lightRed } from "@mui/material/colors";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#db5e59",
+    },
+    secondary: {
+      main: "#ed7470",
+      midNightRed: "#730400",
+    },
+  },
+});
